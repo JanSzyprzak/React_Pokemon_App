6aed005e0333976f6adf82957d90644842cf4182
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-08-04 18:09:46 +0200

    Revert "Add pagination"
    This reverts commit f00dfde77af873f8949d9fd83608ac88d8030e15.

--- Pokemon App/src/App.jsx
+++ Pokemon App/src/App.jsx
@@ -1,52 +1,14 @@
-import { useState, useEffect } from "react";
-import PokemonList from "./PokemonList";
-import Pagination from "./Pagination";
-import axios from "axios";
+import { useState } from 'react'
+
 
 function App() {
-    const [pokemon, setPokemon] = useState([]);
-    const [currentPageUrl, setCurrentPageUrl] = useState(
-        "https://pokeapi.co/api/v2/pokemon"
-    );
-    const [nextPageUrl, setNextPageUrl] = useState()
-    const [prevPageUrl, setPrevPageUrl] = useState()
-    const [loading, setLoading] = useState(true)
-    
+  const [count, setCount] = useState(0)
 
-    useEffect(() => {
-      setLoading(true)
-      let cancel
-        axios.get(currentPageUrl, {
-          cancelToken: new axios.CancelToken(c => cancel = c)
-        }).then((res) => {
-          setLoading(false)
-          setNextPageUrl(res.data.next)
-          setPrevPageUrl(res.data.previous)
-          setPokemon(res.data.results.map((p) => p.name));
-        });
-
-        return () => cancel()
-    }, [currentPageUrl]);
-
-    function gotoNextPage() {
-      setCurrentPageUrl(nextPageUrl)
-    }
-
-    function gotoPrevPage() {
-      setCurrentPageUrl(prevPageUrl)
-    }
-
-    if (loading) return "Loading..."
-
-    return (
-        <>
-            <PokemonList pokemon={pokemon} />
-            <Pagination
-              gotoNextPage={nextPageUrl ? gotoNextPage : null}
-              gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
-            />
-        </>
-    );
+  return (
+    <>
+      
+    </>
+  )
 }
 
-export default App;
+export default App
